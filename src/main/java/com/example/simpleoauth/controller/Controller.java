d9e653603c28fef521607a1f9ec4b75fc96cee14
Kunal113 <kunal@fullstacklabs.com>
2021-03-08 11:55:02 -0500

    Refactored code

--- src/main/java/com/example/simpleoauth/controller/Controller.java
+++ src/main/java/com/example/simpleoauth/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.example.simpleoauth.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @Value("${message: Default Hello}")
+    private String message;
+
     @GetMapping("/access")
     public ResponseEntity<String> home() {
         return ResponseEntity.ok("My resource has been accessed through Okta access token");
+    }
+
+    @GetMapping("/messsage")
+    public String getMessage() {
+        return message;
     }
 }
